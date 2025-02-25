7c01bfe6dffa9500aa3ff3ff7a7c8489fd8d4dc7
KJY <12181467@inha.edu>
2025-02-25 17:42:02 +0900

    v1.0 titanic Assignment

--- day12.py
+++ day12.py
@@ -1,20 +1,34 @@
-# Assignment
-# v0.9) v0.8 파일의 결측치 값을 산술평균으로 채워 넣는 다양한 방법을 적용하시오.
 import numpy as np
 import pandas as pd
 from sklearn.impute import SimpleImputer
+from sklearn.model_selection import train_test_split
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
 
-df = pd.DataFrame(
-    {
-        'A':[1, 2, np.nan, 4],
-        'B':[np.nan, 12, 3, 4],
-        'C':[1, 2, 3, 4]
-    }
-)
+# Titanic 데이터셋 로드
+titanic = sns.load_dataset('titanic')
 
-print(df)
-#i = SimpleImputer(strategy='mean')
-#df[['A', 'B']] = i.fit_transform(df[['A', 'B']])
-df[['A', 'B']] = df[['A', 'B']].fillna(df[['A', 'B']].mean())
-df[['A', 'B']].fillna(df[['A', 'B']].mean(), inplace=True)
-print(df)
+# 'age'가 NaN인 사람들의 나이를 죽은 사람들의 평균 나이로 채우기
+mean_age_of_dead = titanic[titanic['survived'] == 0]['age'].mean()
+titanic['age'] = titanic['age'].fillna(mean_age_of_dead)
+
+X = titanic[['age']]
+y = titanic['survived']
+
+# 결과 확인
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = KNeighborsClassifier(n_neighbors=5)
+
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+plt.figure(figsize=(5, 2))
+plt.scatter(X_test, y_test, color='blue', label='Real')
+plt.scatter(X_test, y_pred, color='red', label='Predicted')
+plt.title('KNeighborsClassifier Real vs Predicted')
+plt.xlabel('Age')
+plt.ylabel('Survived')
+plt.show()
